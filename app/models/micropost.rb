119cda7f2acd171efe1d5853c5615121397886c7
Kelly Chan <ylleknahc@gmail.com>
2015-05-10 18:34:40 -0700

    chapter 2 exercises

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
-	validates :context, length: {maximum: 140}
+	validates :context, length: {maximum: 140}.
+				presence: true
 	belongs_to :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base # arranges for User.all to return all users from database
 	has_many :microposts
+	validates :name, presence: true
+	validates :email, presence: true 
+
 end
